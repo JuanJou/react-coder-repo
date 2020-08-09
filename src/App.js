44f6421eba67d6dccf11c735b9a5a6713ca674ba
Juan Jouglard <jouglardjuan@gmail.com>
2020-08-09 16:11:53 -0300

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Nav } from "./components/Nav";
+import firebase from "firebase";
+import { auth } from "firebase";
+import { createStore } from "redux";
+import reducers from "./redux/store";
+import { Provider } from "react-redux";
+import { Context } from "./context";
 
-function App() {
+function App(props) {
+  const ah = auth();
+  const store = createStore(reducers);
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  ah.onAuthStateChanged((user) => {
+    console.log(user);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Context.Provider value={{ showText: true }}>
+        <div className="App">
+          <Nav></Nav>
+          <button onClick={() => ah.signInWithRedirect(googleProvider)}>
+            Sign in
+          </button>
+        </div>
+      </Context.Provider>
+    </Provider>
   );
 }
 
